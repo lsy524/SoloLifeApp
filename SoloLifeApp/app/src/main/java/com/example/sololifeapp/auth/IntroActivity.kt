a0eb92b141406b65f6136a455dbd1bfe2aa91fa0
sungyong-lim <dlatjddyd524@gmail.com>
2022-08-01 14:33:24 +0900

    Login, Join UI Update

--- SoloLifeApp/app/src/main/java/com/example/sololifeapp/auth/IntroActivity.kt
+++ SoloLifeApp/app/src/main/java/com/example/sololifeapp/auth/IntroActivity.kt
@@ -1,12 +1,34 @@
 package com.example.sololifeapp.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import com.example.sololifeapp.R
+import com.example.sololifeapp.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        onClick(binding.loginBtn)
+        onClick(binding.joinBtn)
+    }
+
+    private fun onClick(v: View) {
+        when(v.id) {
+            R.id.loginBtn -> binding.loginBtn.setOnClickListener {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+
+            R.id.joinBtn -> binding.joinBtn.setOnClickListener {
+                startActivity(Intent(this, JoinActivity::class.java))
+            }
+        }
     }
 }
