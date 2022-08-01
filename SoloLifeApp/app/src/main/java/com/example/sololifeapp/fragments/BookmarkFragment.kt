c9aee71cded62ebf127025f60b1143a51b0e102d
sungyong-lim <dlatjddyd524@gmail.com>
2022-08-01 18:49:53 +0900

    Navigation Bar Update

--- SoloLifeApp/app/src/main/java/com/example/sololifeapp/fragments/BookmarkFragment.kt
+++ SoloLifeApp/app/src/main/java/com/example/sololifeapp/fragments/BookmarkFragment.kt
@@ -5,64 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.sololifeapp.R
+import com.example.sololifeapp.databinding.FragmentBookmarkBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BookmarkFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BookmarkFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentBookmarkBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_bookmark, container, false)
+
+
+
+        onClick(binding.homeTap)
+        onClick(binding.tipTap)
+        onClick(binding.talkTap)
+        onClick(binding.storeTap)
+
+
+        return binding.root
+    }
+
+    private fun onClick(v: View) {
+        when(v.id) {
+            R.id.homeTap -> binding.homeTap.setOnClickListener { it.findNavController().navigate(R.id.action_bookmarkFragment_to_homeFragment) }
+            R.id.tipTap-> binding.tipTap.setOnClickListener{ it.findNavController().navigate(R.id.action_bookmarkFragment_to_tipFragment) }
+            R.id.talkTap -> binding.bookmarkTap.setOnClickListener { it.findNavController().navigate(R.id.action_bookmarkFragment_to_talkFragment) }
+            R.id.storeTap -> binding.storeTap.setOnClickListener { it.findNavController().navigate(R.id.action_bookmarkFragment_to_storeFragment) }
         }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(
-            R.layout.fragment_bookmark,
-            container,
-            false
-        )
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BookmarkFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(
-            param1: String,
-            param2: String
-        ) =
-            BookmarkFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
--- SoloLifeApp/app/src/main/java/com/example/sololifeapp/fragments/HomeFragment.kt
+++ SoloLifeApp/app/src/main/java/com/example/sololifeapp/fragments/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.sololifeapp.R
 import com.example.sololifeapp.databinding.FragmentHomeBinding
 
@@ -22,11 +23,23 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
-        binding.tipTap.setOnClickListener{
-            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
-        }
+
+
+        onClick(binding.tipTap)
+        onClick(binding.talkTap)
+        onClick(binding.bookmarkTap)
+        onClick(binding.storeTap)
 
 
         return binding.root
+    }
+
+    private fun onClick(v: View) {
+        when(v.id) {
+            R.id.tipTap-> binding.tipTap.setOnClickListener{ it.findNavController().navigate(R.id.action_homeFragment_to_tipFragment) }
+            R.id.talkTap -> binding.talkTap.setOnClickListener { it.findNavController().navigate(R.id.action_homeFragment_to_talkFragment) }
+            R.id.bookmarkTap -> binding.bookmarkTap.setOnClickListener { it.findNavController().navigate(R.id.action_homeFragment_to_bookmarkFragment) }
+            R.id.storeTap -> binding.storeTap.setOnClickListener { it.findNavController().navigate(R.id.action_homeFragment_to_storeFragment) }
+        }
     }
 }
