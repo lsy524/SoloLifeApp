8bcc62a51df3247f9e9f88a33037dd64669f63a0
sungyong-lim <dlatjddyd524@gmail.com>
2022-08-01 13:20:56 +0900

    Splash page postDelayed Update

--- SoloLifeApp/app/src/main/java/com/example/sololifeapp/SplashActivity.kt
+++ SoloLifeApp/app/src/main/java/com/example/sololifeapp/SplashActivity.kt
@@ -1,11 +1,22 @@
 package com.example.sololifeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.sololifeapp.auth.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // 3초 후에 IntroActivity 로 이동
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 3000)
+
+
     }
 }
