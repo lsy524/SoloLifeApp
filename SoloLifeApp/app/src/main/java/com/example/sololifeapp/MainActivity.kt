f39dc3d791cee3971a96ddfc039dbb8cbb983160
sungyong-lim <dlatjddyd524@gmail.com>
2022-08-01 17:57:51 +0900

    Create Navigation Bar Setting

--- SoloLifeApp/app/src/main/java/com/example/sololifeapp/MainActivity.kt
+++ SoloLifeApp/app/src/main/java/com/example/sololifeapp/MainActivity.kt
@@ -1,25 +1,14 @@
 package com.example.sololifeapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.databinding.DataBindingUtil
-import com.example.sololifeapp.auth.IntroActivity
-import com.example.sololifeapp.databinding.ActivityMainBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        auth = Firebase.auth
 
 //         Logout
 //        binding.logoutBtn.setOnClickListener {
