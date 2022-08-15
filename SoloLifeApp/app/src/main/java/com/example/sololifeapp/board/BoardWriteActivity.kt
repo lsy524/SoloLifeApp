7c8c1d859f7af05dbb02772297a42afc1b4385e4
dlatjddyd524@gmail.com <dlatjddyd524@gmail.com>
2022-08-15 17:45:34 +0900

    Update imageButton onClick gallery

--- SoloLifeApp/app/src/main/java/com/example/sololifeapp/board/BoardWriteActivity.kt
+++ SoloLifeApp/app/src/main/java/com/example/sololifeapp/board/BoardWriteActivity.kt
@@ -2,8 +2,10 @@ package com.example.sololifeapp.board
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
@@ -28,25 +30,30 @@ class BoardWriteActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_board_write)
 
         // 화면 터치 시 키보드를 숨기는 코드
-//        binding.root.setOnTouchListener { _, _ ->
-//            hideKeyboard()
-//        }
+        /*
+        binding.root.setOnTouchListener { _, _ ->
+            hideKeyboard()
+        }
+        */
 
         onClick(binding.writeBtn)
+        onClick(binding.imageArea)
 
     }
 
     // 키보드를 숨기는 메서드
-//    private fun hideKeyboard() : Boolean{
-//        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//        imm.hideSoftInputFromWindow(this.currentFocus!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
-//
-//        return false
-//    }
+    /*
+    private fun hideKeyboard() : Boolean{
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(this.currentFocus!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+
+        return false
+    }
+    */
 
     private fun onClick(v: View) {
         when(v.id) {
-            // 게시글 작성 완료 버튼 클릭 시 발생하는 메서드
+            // 게시글 작성 완료 버튼 클릭 시 실행되는 코드
             R.id.writeBtn -> binding.writeBtn.setOnClickListener {
                 val title = binding.titleArea.text.toString() // 입력한 글 제목
                 val content = binding.contentArea.text.toString() // 입력한 글 내용
@@ -67,6 +74,24 @@ class BoardWriteActivity : AppCompatActivity() {
 
                 finish()
             }
+
+            // 이미지 업로드 버튼 클릭 시 실행되는 코드
+            R.id.imageArea -> binding.imageArea.setOnClickListener {
+                // 갤러리 이동 1-2 버튼 클릭 시 갤러리(사진첩)으로 이동하는 코드
+                val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+                startActivityForResult(gallery, 100)
+            }
+        }
+    }
+
+    // 갤러리 이동 1-1
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // 갤러리 이동 1-3
+        if(resultCode == RESULT_OK && requestCode == 100) {
+
+            // 선택한 사진을 imageArea 에 저장하는 코드
+            binding.imageArea.setImageURI(data?.data)
         }
     }
 
