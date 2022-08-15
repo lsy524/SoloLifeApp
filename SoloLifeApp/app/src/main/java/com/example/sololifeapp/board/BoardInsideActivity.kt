f161a0d78bcb6eb86e80affabde152940bbf6c8a
dlatjddyd524@gmail.com <dlatjddyd524@gmail.com>
2022-08-15 17:28:14 +0900

    Update Get board data from database

--- SoloLifeApp/app/src/main/java/com/example/sololifeapp/board/BoardInsideActivity.kt
+++ SoloLifeApp/app/src/main/java/com/example/sololifeapp/board/BoardInsideActivity.kt
@@ -3,9 +3,15 @@ package com.example.sololifeapp.board
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.sololifeapp.R
 import com.example.sololifeapp.databinding.ActivityBoardInsideBinding
+import com.example.sololifeapp.model.BoardModel
+import com.example.sololifeapp.util.FBRef
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 class BoardInsideActivity : AppCompatActivity() {
 
@@ -16,17 +22,46 @@ class BoardInsideActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_board_inside)
 
+
+        // 방법 1. listView 에 있는 데이터 title, content, time 을 다 다른 액티비티로 전달해줘서 만들기
+        /*
+        // 방법 1-2 보내준 데이터를 받음
         val title = intent.getStringExtra("title").toString()
         val content = intent.getStringExtra("content").toString()
         val time = intent.getStringExtra("time").toString()
 
-//        Log.d(TAG, title)
-//        Log.d(TAG, content)
-//        Log.d(TAG, time)
-
+        // 방법 1-3 받을 데이터를 해당 요소에 할당
         binding.titleArea.text = title
         binding.contentArea.text = content
         binding.timeArea.text = time
+         */
 
+        // 방법 2-4 보내준 데이터를 받음
+        val key = intent.getStringExtra("key").toString()
+//        Toast.makeText(this, key, Toast.LENGTH_SHORT).show()
+        getBoardData(key)
+    }
+
+    // 방법 2-5 받은 데이터를 토대로 데이터를 가져옴
+    private fun getBoardData(key: String) {
+        // 데이터베이스에서 게시글 데이터를 가져오는 코드
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // 방법 2-7 BoardModel 형태로 데이터를 가져옴
+                // 데이터 하나만 가져오면 되기 때문에 for 문 실행 X
+                val dataModel = dataSnapshot.getValue(BoardModel::class.java) // BoardModel 형태로 데이터를 받는 다는 의미
+
+                // 방법 2-8 데이터를 해당 view 에 할당
+                binding.titleArea.text = dataModel!!.title
+                binding.contentArea.text = dataModel.content
+                binding.timeArea.text = dataModel.time
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+        }
+        // 방법 2-6 board filed 에 key 값을 가리키게 만듬
+        FBRef.boardRef.child(key).addValueEventListener(postListener)
     }
 }
--- SoloLifeApp/app/src/main/java/com/example/sololifeapp/fragments/TalkFragment.kt
+++ SoloLifeApp/app/src/main/java/com/example/sololifeapp/fragments/TalkFragment.kt
@@ -29,6 +29,8 @@ class TalkFragment : Fragment() {
 
     private lateinit var boardRVAdapter : BoardListLVAdapter
 
+    private val boardKeyList = mutableListOf<String>() // 방법 2-1 board 에 키 값을 담을 리스트 변수 생성
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -46,12 +48,19 @@ class TalkFragment : Fragment() {
         binding.boardListView.adapter = boardRVAdapter // 어댑터를 boardListView 와 연결
 
         // 게시글 클릭 시 BoardModel(title, content, time)을 다른 액티비티로 보내는 방법
-        // 방법 1. listView 에 있는 데이터 title, content, time 을 다 다른 액티비티로 전달해줘서 만들기
         binding.boardListView.setOnItemClickListener { parent, view, position, id ->
             val intent = Intent(requireContext(), BoardInsideActivity::class.java)
-            intent.putExtra("title",boardDataList[position].title)
+
+            // 방법 1. listView 에 있는 데이터 title, content, time 을 다 다른 액티비티로 전달해줘서 만들기
+            /*
+            // 방법 1-1 intent 로 boardDataList 에 있는 항목들을 한개씩 넘겨줌
+            intent.putExtra("title",boardDataList[position].title) 
             intent.putExtra("content",boardDataList[position].content)
             intent.putExtra("time",boardDataList[position].time)
+            */
+
+            // 방법 2. 파이어베이스에 있는 board(field)에 대한 데이터의 id를 기반으로 다시 데이터를 받아오는 방법
+            intent.putExtra("key",boardKeyList[position]) // 방법 2-3 intent 로 boardKeyList 를 보내줌
             startActivity(intent)
         }
 
@@ -82,10 +91,11 @@ class TalkFragment : Fragment() {
                 for (dataModel in dataSnapshot.children) {
                     val item = dataModel.getValue(BoardModel::class.java) // 데이터를 boardModel 형태로 출력
                     boardDataList.add(item!!) // boardDataList 에 추가
+                    boardKeyList.add(dataModel.key.toString()) // 방법 2-2. 키 값을 boardKeyList 에 추가
 
                 }
-                // Log.d(TAG, boardDataList.toString())
                 boardDataList.reverse()
+                boardKeyList.reverse()
                 boardRVAdapter.notifyDataSetChanged()
 
             }
